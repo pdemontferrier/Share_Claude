f3f23f6ba773129efd09cc9fdf92ca6cdf63000c
Admin <montferrier.patrick@gmail.com>
2026-06-19 12:04:37 +0200

    Update

--- DG244Cutting/E_Miscellaneous/CompositionRoot/SR_ConteneurDI.cs
+++ DG244Cutting/E_Miscellaneous/CompositionRoot/SR_ConteneurDI.cs
@@ -349,6 +349,14 @@ namespace DG244Cutting.E_Miscellaneous.CompositionRoot
             //   du 0230, R-4.10.14 du 0231). Validation par
             //   ValidateScopes=true en build DEBUG.
             services.AddSingleton<VM_MH_Reduce>();
+            services.AddSingleton<VM_MH10>();
+            services.AddSingleton<VM_MH20>();
+            services.AddSingleton<VM_MH30>();
+            services.AddSingleton<VM_MH40>();
+            services.AddSingleton<VM_MH50>();
+            services.AddSingleton<VM_MH60>();
+            services.AddSingleton<VM_MH70>();
+            services.AddSingleton<VM_MH80>();
             services.AddSingleton<VM_MH99>();
 
             // Generic
